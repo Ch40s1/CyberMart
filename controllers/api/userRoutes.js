6e4a1ca729660ba5c0e28bb7e0bc63d36a17ddaa
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-09-13 10:54:27 -0500

    users are now able to log out and create login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// router.post('/', async (req, res) => {
-//   try {
-//     const userData = await User.create(req.body);
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
 
-//     req.session.save(() => {
-//       req.session.user_id = userData.id;
-//       req.session.user_name = userData.username;
-//       req.session.logged_in = true;
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.user_name = userData.username;
+      req.session.logged_in = true;
 
-//       res.status(200).json(userData);
-//     });
-//   } catch (err) {
-//     console.error(err); // Log the error for debugging
-//     res.status(500).json({ message: 'Internal server error' });
-//   }
-// });
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    console.error(err); // Log the error for debugging
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 router.post('/login', async (req, res) => {
   try {
@@ -47,14 +47,14 @@ router.post('/login', async (req, res) => {
   }
 });
 
-// router.post('/logout', (req, res) => {
-//   if (req.session.logged_in) {
-//     req.session.destroy(() => {
-//       res.status(204).end();
-//     });
-//   } else {
-//     res.status(404).end();
-//   }
-// });
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
