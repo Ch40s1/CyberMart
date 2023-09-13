5c61b81465cc0c91ee5c30cd56b32de14dbe581f
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-09-13 13:02:03 -0500

    added cart icon and implemented light and dark mode

---
+++ public/js/colorMode.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  const modeToggle = document.getElementById('mode-toggle');
+  const body = document.body;
+
+  modeToggle.addEventListener('click', function () {
+    body.classList.toggle('light-mode');
+  });
+});
