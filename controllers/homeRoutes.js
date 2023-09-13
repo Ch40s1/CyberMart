a377a100f1b148d3c8029ec13e9a9d9051c43753
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-09-13 18:57:29 -0500

    deleted unsed code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -43,28 +43,6 @@ router.get('/', async (req, res) => {
 //   }
 // });
 
-// router.get('/project/:id', async (req, res) => {
-//   try {
-//     const projectData = await Project.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
-
-//     const project = projectData.get({ plain: true });
-
-//     res.render('project', {
-//       ...project,
-//       logged_in: req.session.logged_in,
-//       user_name: req.session.user_name,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 // Define a route for rendering the cart page
 router.get('/cart', (req, res) => {
   // Render the "cart.hbs" template without passing any data
